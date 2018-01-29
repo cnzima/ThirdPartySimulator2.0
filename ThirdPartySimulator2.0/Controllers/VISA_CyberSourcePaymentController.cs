2ac2a4fb52e6d7d70065678b925d7c81ef3bb806
CNzima <CNzima@CNZI-NB-01>
2018-01-29 15:37:06 +0200

    Changing PayTrace Control and Request to eft processing instead of card processing

--- ThirdPartySimulator2.0/Controllers/VISA_CyberSourcePaymentController.cs
+++ ThirdPartySimulator2.0/Controllers/VISA_CyberSourcePaymentController.cs
@@ -14,6 +14,7 @@ namespace ThirdPartySimulator2._0.Controllers
 {
     public class VISA_CyberSourcePaymentController : ApiController
     {
+
         // GET: api/VISA_CyberSourcePayment
         public IEnumerable<string> Get()
         {
--- ThirdPartySimulator2.0/Models/PayTraceRequest.cs
+++ ThirdPartySimulator2.0/Models/PayTraceRequest.cs
@@ -5,18 +5,13 @@ using System.Web;
 
 namespace ThirdPartySimulator2._0.Models
 {
-    public class PayTraceRequest
+    public class PayTraceRequest //eft request
     {
         public double amount { get; set; }
 
-        public string currency { get; set; }
-
         public string number { get; set; }
 
-        public string expiration_month { get; set; }
+        public string holder { get; set; }//Added account holder.
 
-        public string expiration_year { get; set; }
-
-        public string csc { get; set; }
     }
 }
